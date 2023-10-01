aa6aa7ec4633f43aa41c79c9e2859495590212e7
ramdhanyA <ariqathallah38@gmail.com>
2023-10-02 08:28:51 +0900

    added input folder and output folder

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import PptxGenJS from "pptxgenjs";
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
 function shuffleArray<T>(originalArray: ReadonlyArray<T>): T[] {
 	let array = [...originalArray];  // Create a copy
@@ -12,30 +13,52 @@ function shuffleArray<T>(originalArray: ReadonlyArray<T>): T[] {
 	return array;
 }
 
-let csvContent: string = fs.readFileSync('./N2.csv', 'utf8');
-let rows: string[] = csvContent.split('\n').slice(0, -1);
+const folderPath = './input';
 
-const randRows = shuffleArray(rows);
+const files: string[] = fs.readdirSync(folderPath);
+
+files.forEach(file => {
+	const filePath = path.join(folderPath, file);
+
+	let csvContent: string = fs.readFileSync(filePath, 'utf8');
+	let rows: string[] = csvContent.split('\n').slice(0, -1);
+
+	const randRows = shuffleArray(rows);
 
 
-let index = 0;
-const numOfWordsPerPpt = 100;
-while (index < randRows.length) {
+	let index = 0;
+	const numOfWordsPerPpt = 100;
+	while (index < randRows.length) {
 
-	let pptx = new PptxGenJS();
-	for (const row of randRows.slice(index, index + numOfWordsPerPpt > randRows.length ? randRows.length : index + numOfWordsPerPpt)) {
-		let slide = pptx.addSlide();
-		const card = row.split(',').join('/n');
-		slide.addText(`${card}`, {
-			x: 0,
-			y: 1,
-			w: "100%",
-			h: 2,
-			align: "center",
-			color: "000000",
-			fontSize: 48,
-		});
+		let pptx = new PptxGenJS();
+		for (let row of randRows.slice(index, index + numOfWordsPerPpt > randRows.length ? randRows.length : index + numOfWordsPerPpt)) {
+			let slide = pptx.addSlide();
+			row = row.replaceAll(",\"", "|")
+			let [kanji, reading, meaning] = row.split('|');
+			kanji = kanji.replaceAll("\"", "");
+			reading = reading.replaceAll("\"", "");
+			meaning = meaning.replaceAll("\"", "");
+			const card = `${kanji}\n${reading}`
+			slide.addText(`${card}`, {
+				x: 0,
+				y: 1,
+				w: "100%",
+				h: 2,
+				align: "center",
+				color: "000000",
+				fontSize: 48,
+			});
+			slide.addText(`${meaning.slice(0, 121)}...`, {
+				x: 0,
+				y: 3,
+				w: "100%",
+				h: 2,
+				align: "center",
+				color: "000000",
+				fontSize: 36,
+			})
+		}
+		pptx.writeFile({ fileName: `./output/${file}.pptx` });
+		index += numOfWordsPerPpt;
 	}
-	pptx.writeFile({ fileName: `./output/PptxGenJS-Demo${index}.pptx` });
-	index += numOfWordsPerPpt;
-}
+});
